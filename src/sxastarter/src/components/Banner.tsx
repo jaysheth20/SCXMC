a87143a63db80557b004ecd97e35b685807766f5
jay sheth <jay@sourceved.com>
2024-09-24 14:56:51 +0530

    changes

--- src/sxastarter/src/components/Banner.tsx
+++ src/sxastarter/src/components/Banner.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Field, ImageField, Text ,Image as JssImage } from '@sitecore-jss/sitecore-jss-nextjs';
-
+import { Field, ImageField, Text, Image as JssImage } from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
-    Heading: Field<string>;
-    Image:ImageField;
+  Heading: Field<string>;
+  Image: ImageField;
 }
 
 export type BannerProps = {
@@ -12,12 +11,12 @@ export type BannerProps = {
 };
 
 export const banner = (props: BannerProps): JSX.Element => {
-return(
-<div className="container-default">
-    <h1 className="component title row">
-        <Text field={props.fields.Heading} />
-        <JssImage field={props.fields.Image} />
-    </h1>
-</div>
-);
+    return(
+        <div className="container-default">
+            <h1 className="component title row">
+                <Text field={props.fields.Heading} />
+                <JssImage field={props.fields.Image} />
+            </h1>
+        </div>
+    );
 };
