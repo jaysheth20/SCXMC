39131b31e68dfe96bc2d80c678815e90f8002bb9
jay sheth <jay@sourceved.com>
2024-09-24 14:36:32 +0530

    banner component added

---
+++ src/sxastarter/src/components/Banner.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Field, ImageField,Text,Image as JssImage } from '@sitecore-jss/sitecore-jss-nextjs';
+
+
+interface Fields {
+    Heading: Field<string>;
+    Image:ImageField;
+}
+
+export type BannerProps = {
+    fields: Fields;
+};
+
+export const banner = (props:BannerProps): JSX.Element => {
+return(
+<div className="container-default">
+    <h1 className="component title row">
+        <Text field={props.fields.Heading} />
+        <JssImage field={props.fields.Image} />
+    </h1>
+</div>
+);
+};
