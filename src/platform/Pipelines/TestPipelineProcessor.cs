4baf067cb47280ffe525242d0a09d80f3bab48e1
dhruv trivedi <dhruvtrivedi2002@gmail.com>
2025-08-19 15:42:01 +0530

    config error

--- src/platform/Pipelines/TestPipelineProcessor.cs
+++ src/platform/Pipelines/TestPipelineProcessor.cs
@@ -1,16 +1,16 @@
-﻿using Sitecore.Diagnostics;
-using Sitecore.Pipelines;
+﻿//using Sitecore.Diagnostics;
+//using Sitecore.Pipelines;
 
-namespace XmCloudSXAStarter.Pipelines
-{
-    public class TestPipelineProcessor
-    {
-        public void Process(PipelineArgs args)
-        {
-            Log.Info("🚀 TestPipelineProcessor executed!", this);
+//namespace XmCloudSXAStarter.Pipelines
+//{
+//    public class TestPipelineProcessor
+//    {
+//        public void Process(PipelineArgs args)
+//        {
+//            Log.Info("🚀 TestPipelineProcessor executed!", this);
 
-            // You can add extra checks, modify args, etc.
-            args.CustomData["ExecutedAt"] = System.DateTime.Now;
-        }
-    }
-}
+//            // You can add extra checks, modify args, etc.
+//            args.CustomData["ExecutedAt"] = System.DateTime.Now;
+//        }
+//    }
+//}
